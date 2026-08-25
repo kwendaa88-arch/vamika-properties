e2f830031a463beb9d27c79824fb0e8583bba63a
kwendaa88-arch <kwendaa88@gmail.com>
2026-08-25 16:08:24 +0530

    Update trustedproxy.php

--- config/trustedproxy.php
+++ config/trustedproxy.php
@@ -2,5 +2,9 @@
 
 return [
     'proxies' => '*',
-    'headers' => Illuminate\Http\Middleware\TrustProxies::HEADER_X_FORWARDED_ALL,
+    'headers' => Illuminate\Http\Middleware\TrustProxies::HEADER_X_FORWARDED_FOR |
+                 Illuminate\Http\Middleware\TrustProxies::HEADER_X_FORWARDED_HOST |
+                 Illuminate\Http\Middleware\TrustProxies::HEADER_X_FORWARDED_PORT |
+                 Illuminate\Http\Middleware\TrustProxies::HEADER_X_FORWARDED_PROTO |
+                 Illuminate\Http\Middleware\TrustProxies::HEADER_X_FORWARDED_AWS_ELB,
 ];
